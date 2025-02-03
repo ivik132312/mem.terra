1a98374018066884ad2ab43b326d85e4e321e777
ivik132312 <isabashev@yandex.ru>
2025-02-03 19:11:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const canvas = document.getElementById('game')
+const ctx = canvas.getContext('2d')
+
+const vragAva=new Image()
+vragAva.src='i.webp'
+
+const playerAva=new Image()
+playerAva.src='cola.png'
+playerAva.width=50
+playerAva.height=50
+let player = { x: 50, y: 50, width: 50, height: 50, speed: 5, img:playerAva};
+let vrag = { x: 650, y: 50, width: 80, height: 80, speed: 15, img:vragAva};
+function update() {
+    if (keys['KeyW']) player.y -= player.speed;
+    if (keys['KeyS']) player.y += player.speed;
+    if (keys['KeyA']) player.x -= player.speed;
+    if (keys['KeyD']) player.x += player.speed;
+    if (keys['KeyI']) vrag.y -= vrag.speed;
+    if (keys['KeyK']) vrag.y += vrag.speed;
+    if (keys['KeyJ']) vrag.x -= vrag.speed;
+    if (keys['KeyL']) vrag.x += vrag.speed;
+
+    // Границы поля
+    if (player.x < 0) player.x = 0;
+    if (player.x + player.width > canvas.width) player.x = canvas.width - player.width;
+    if (player.y < 0) player.y = 0;
+    if (player.y + player.height > canvas.height) player.y = canvas.height - player.height;
+    if (vrag.x < 0) vrag.x = 0;
+    if (vrag.x + vrag.width > canvas.width) vrag.x = canvas.width - vrag.width;
+    if (vrag.y < 0) vrag.y = 0;
+    if (vrag.y + vrag.height > canvas.height) vrag.y = canvas.height - vrag.height;
+}
+// Рисование игрока 
+function draw() { 
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    ctx.drawImage(player.img, player.x, player.y, player.width, player.height)
+    ctx.drawImage(vrag.img,  vrag.x, vrag.y, vrag.width, vrag.height)
+    if(isCollision(player,vrag)){
+        player.x=50
+        player.y=50
+        vrag.x=650
+        vrag.y=50
+        alert('УДАР!')
+    }
+}
+// Основной игровой цикл 
+function gameLoop() { 
+    update(); draw(); requestAnimationFrame(gameLoop); 
+}
+// Управление клавишами 
+let keys = {}; 
+window.addEventListener('keydown', function (e) { keys[e.code] = true; });
+window.addEventListener('keyup', function (e) { keys[e.code] = false; });
+// Запуск игры 
+playerAva.onload=function(){
+    player.width=playerAva.width
+    player.height=playerAva.height
+    gameLoop();
+}
+function isCollision(player,vrag){
+    return (
+        player.x<vrag.x+vrag.width&&
+        player.x+player.width>vrag.x&&
+        player.y<vrag.y+vrag.height&&
+        player.y+player.height>vrag.y
+    )
+}
